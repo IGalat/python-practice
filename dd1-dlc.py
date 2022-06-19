2b38ffdcf41437c4eca56cf39327121bd68ed6d9
Illia Halatiuk <galatyuk.ilya@gmail.com>
2022-06-19 14:24:32 +0700

    init2

---
+++ dd1-dlc.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+
+
+packIncludes = "Dungeon Defenders, Dungeon Defenders - Quest for the Lost Eternia Shards Part 4, Dungeon Defenders - Etherian Holiday Extravaganza, Dungeon Defenders - Karathiki Jungle Mission Pack, Dungeon Defenders - President's Day Surprise , Dungeon Defenders - Quest for the Lost Eternia Shards Part 1, Dungeon Defenders - Quest for the Lost Eternia Shards Part 2, Dungeon Defenders - The Tinkerer's Lab Mission Pack, Dungeon Defenders Anniversary Pack, Dungeon Defenders Halloween Costume Pack, Dungeon Defenders Halloween Mission Pack, Dungeon Defenders Lucky Costume Pack, Dungeon Defenders New Heroes DLC, Dungeon Defenders The Great Turkey Hunt! Mission & Costumes, Dungeon Defenders Warping Core Challenge Mission Pack, Dungeon Defenders: Assault Mission Pack, Dungeon Defenders: Barbarian Hero DLC, Dungeon Defenders: City in the Cliffs Mission Pack, Dungeon Defenders: Etherian Festival of Love, Dungeon Defenders: Jester Hero DLC, Dungeon Defenders: Penny Arcade Character Pack, Dungeon Defenders: Quest for the Lost Eternia Shards Part 3, Dungeon Defenders: Series EV Hero DLC, Dungeon Defenders: Summoner Hero DLC, Dungeon Defenders: Talay Mining Complex Mission Pack"
+
+allDlcsListed = """
+-75%Rp 17 499Rp 4 374
+DUNGEON DEFENDERS - THE TINKERER'S LAB MISSION PACK 27 Jun, 2013
+Embedded within the floating islands of Sky City, the Tinkerer's Lab is the birthplace of the Series EV. Her creator, the Tinkerer, has been preparing for a chasis & software upgrade to EV 1.5, but harnessing the Eternia energy has attracted unwanted attention!
+
+-75%Rp 17 499Free
+DUNGEON DEFENDERS ANNIVERSARY PACK 17 Oct, 2012
+Celebrate Dungeon Defenders 1 year anniversary and defend the Tavern with the Dungeon Defenders Anniversary pack! This DLC contains: Tavern Defenders Campaign Mission Party Hats Accessories Birthday Cake Crystal Core Kobold on a Treadmill Pet
+
+-80%Rp 32 999Rp 6 599
+DUNGEON DEFENDERS: JESTER HERO DLC 29 Aug, 2012
+A sly trickster born in a whimsical realm of chaos, the Jester's abilities are shrouded in mystery. She can quickly turn the tide of battle by using any weapon and can deceive enemies with 'presents' that explode to reveal random defenses and bonuses.
+
+-80%Rp 32 999Rp 6 599
+DUNGEON DEFENDERS - QUEST FOR THE LOST ETERNIA SHARDS PART 4 16 Dec, 2011
+New Campaign missions, bosses, costumes, weapons, pets and challenges in the epic conclusion to the Quest for the Lost Eternia Shards.
+
+-75%Rp 17 499Rp 4 374
+DUNGEON DEFENDERS: TALAY MINING COMPLEX MISSION PACK 1 Jun, 2012
+Among the sparkling gemstones of Talay, a set of long-buried Eternia Crystals have been unearthed. But the volcanic activity of the mine is rapidly rising and its imminent eruption threatens to destroy both friend and foe.
+
+-80%Rp 32 999Rp 6 599
+DUNGEON DEFENDERS: SUMMONER HERO DLC 24 May, 2012
+A mysterious visitor from the Crystalline dimension, the Summoner has decided to wage his own war across Etheria! With two pets by his side, the Summoner can form minions out of sentient crystal and build an army to follow his every command!
+
+-80%Rp 32 999Rp 6 599
+DUNGEON DEFENDERS: QUEST FOR THE LOST ETERNIA SHARDS PART 3 16 Dec, 2011
+Now that your Dungeon’s been defended, prepare to venture outside into the realms of Etheria in The Quest for the Lost Eternia Shards! Explore the underwater city of Aquanos and experience 4 all new missions, story cinematics, enemies, a whole new boss, and more: Key Features: The Quest for the...
+
+-75%Rp 17 499Rp 4 374
+DUNGEON DEFENDERS: CITY IN THE CLIFFS MISSION PACK 14 Apr, 2012
+An isolated Moraggan outpost has now become a warzone as the horde lays siege to its Eternia Crystals...
+
+-75%Rp 17 499Rp 4 374
+DUNGEON DEFENDERS: PENNY ARCADE CHARACTER PACK 6 Apr, 2012
+Penny Arcade characters have invaded Dungeon Defenders! You can now play as your favorite Penny Arcade characters, Tycho, Cardboard Tube Samurai, Annarchy, and Jim DarkMagic, in Dungeon Defenders! If you look around hard enough, you might even find your very own cardboard tube sword!
+
+-80%Rp 32 999Rp 6 599
+DUNGEON DEFENDERS: SERIES EV HERO DLC 30 Mar, 2012
+Assembled long ago by a master mechanic within the depths of Sky City, the Series EV automaton has been powered back to life and is ready for action! By placing down an array of powerful Electronic Beams of any length, EV can decimate enemy forces.
+
+-75%Rp 17 499Rp 4 374
+DUNGEON DEFENDERS - KARATHIKI JUNGLE MISSION PACK 16 Dec, 2011
+Venture into the ancient ruins of the Karathiki Jungles to discover Dungeon Defenders first 6-player co-op map, the Amazon Barbarian tribe and more! This DLC also includes access to the Karathiki Competitive Tower Defense Mission, where you can compete with up 6 heroes (3 on a team) to see who can...
+
+-50%Rp 8 499Rp 4 249
+DUNGEON DEFENDERS LUCKY COSTUME PACK 17 Mar, 2012
+This time we let you, our community, vote on the character classes and costumes for our Holiday costume pack! So… get ready to celebrate St. Patrick’s Day with these awesome player-chosen costume winners!
+
+-80%Rp 32 999Rp 6 599
+DUNGEON DEFENDERS - QUEST FOR THE LOST ETERNIA SHARDS PART 2 16 Dec, 2011
+Now that your Dungeon’s been defended, prepare to venture outside into the realms of Etheria in The Quest for the Lost Eternia Shards! Explore the desert city of Moraggo and experience 3 all new missions, story cinematics, enemies, a whole new boss, and more: This DLC pack contains: 'The Quest for...
+
+-75%Rp 17 499Rp 4 374
+DUNGEON DEFENDERS - PRESIDENT'S DAY SURPRISE 16 Mar, 2012
+Celebrate President’s Day in Etheria with the “President’s Day: Battle Royale” holiday event!
+
+-75%Rp 17 499Rp 4 374
+DUNGEON DEFENDERS: ETHERIAN FESTIVAL OF LOVE 15 Feb, 2012
+Celebrate Valentine’s Day in Etheria with the “Etherian Festival of Love” holiday event!
+
+-75%Rp 17 499Rp 4 374
+DUNGEON DEFENDERS: ASSAULT MISSION PACK 16 Dec, 2011
+Done defending and ready to attack? This Dungeon Defenders Mission Pack contains 3 new assault missions for you to tackle with your friends as well as new weapons and a unique pet that can only be obtained by beating these missions!
+
+-80%Rp 24 999Rp 4 999
+DUNGEON DEFENDERS: BARBARIAN HERO DLC 1 Feb, 2012
+Hailing from the dense jungle lands far to the south, the Barbarian is a masterful fighter who can wield two melee weapons simultaneously and unleash devastating combos! Using five distinct fighting stances instead of towers, the Barbarian is able to adapt to any combat situation.
+
+-75%Rp 17 499Rp 4 374
+DUNGEON DEFENDERS - ETHERIAN HOLIDAY EXTRAVAGANZA 21 Dec, 2011
+Celebrate the Winter Holidays in Etheria with the Etherian Holiday Extravaganza! event!
+
+-80%Rp 32 999Rp 6 599
+DUNGEON DEFENDERS - QUEST FOR THE LOST ETERNIA SHARDS PART 1 16 Dec, 2011
+Venture outside into the realms of Etheria in The Quest for the Lost Eternia Shards!
+
+-75%Rp 17 499Rp 4 374
+DUNGEON DEFENDERS THE GREAT TURKEY HUNT! MISSION & COSTUMES 14 Dec, 2011
+Celebrate your Thanksgiving in Etheria with 'The Great Turkey Hunt!' event on TrendyNet! Hunt humongous herds of turkeys for their delicious Thanksgiving meat… but watch out! There are dangerous Dark Elf Warriors who will stop at nothing to protect the turkeys from harm and lurking in the shadows,...
+
+-75%Rp 17 499Rp 4 374
+DUNGEON DEFENDERS WARPING CORE CHALLENGE MISSION PACK 24 Nov, 2011
+Ready for even more of a challenge? This Dungeon Defenders Challenge pack contains 4 new warping core missions for you to tackle with your friends as well as unique loot that can only be obtained by beating these missions!
+
+-80%Rp 39 999Rp 7 999
+DUNGEON DEFENDERS NEW HEROES DLC 24 Nov, 2011
+Want 4 new Dungeon Defenders Hero Classes? Want a chance to play all 8 at a time in the new 8-player PvE Uber Monster Fest Challenge mission? Well get ready. This pack contains gender-swapped versions of the main characters, equipped with all new abilities, stat ramps, and more: The Initiate A...
+
+Free
+DUNGEON DEFENDERS DEVELOPMENT KIT (FREE DLC) 12 Nov, 2011
+The Dungeon Defenders Development Kit is a robust set of tools for players to modify all aspects of the Dungeon Defenders source and create their own content and games. Packaged with the game’s complete source code and art assets, the Dungeon Defenders Development Kit is one of the definitive...
+
+Free
+DUNGEON DEFENDERS CAPTURE THE FLAG PRE-ALPHA PASS (FREE DLC) 12 Nov, 2011
+This pass grants you early access to Dungeon Defenders’ 16-player Capture the Flag mode. Here you must form an 8-man team and score the most points by killing opponents and holding the flag for the longest time. The team with the most points will win.
+
+-75%Rp 17 499Rp 4 374
+DUNGEON DEFENDERS HALLOWEEN MISSION PACK 12 Nov, 2011
+Celebrate Halloween with everyone on TrendyNet! Now updated for 2012! This Halloween Mission Pack contains: 1 new Halloween-themed mission 8 exclusive Halloween weapons 4 ultra-rare, unlockable character skins: Ninja - Monk Vampire - Apprentice Death Knight - Squire Angel - Huntress And New For...
+
+-50%Rp 8 499Rp 4 249
+DUNGEON DEFENDERS HALLOWEEN COSTUME PACK 12 Nov, 2011
+Want to spice up your heroes? This pack includes 4 scary outfits for your TrendyNet Dungeon Defenders characters: Devil – Monk Mummy – Apprentice Pirate – Squire Witch – Huntress
+"""
+
+packDlcs = [dlc.strip() for dlc in packIncludes.split(',')]
+allDlcs = allDlcsListed.split('\n\n')
+
+for packDlc in packDlcs:
+    packDlc = packDlc.upper()
+    for dlc in allDlcs:
+        if packDlc in dlc:
+            allDlcs.remove(dlc)
+
+print('\n\n'.join(allDlcs))
---
+++ gemcraft-fw-hidden-mod.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+hidden_mod_keys_string = '''1 3 8 7 12
+4 7 5 10 1
+8 10 12 8 3
+11 4 8 4 1
+5 3 3 5 6
+12 2 4 3 1
+2 9 10 7 12
+1 7 4 12 3
+1 2 10 10 4
+6 11 4 7 1
+2 5 9 9 1
+8 3 8 4 1
+10 4 5 12 2
+11 2 9 4 7
+1 7 8 8 10
+4 10 4 10 1
+3 6 8 9 8
+5 6 2 6 1
+12 3 6 10 9
+2 8 3 6 11
+1 11 10 5 3
+7 12 8 10 1
+1 5 8 4 7
+10 8 11 2 1
+3 5 6 5 1
+7 12 8 2 5
+9 11 3 3 5
+1 11 12 9 11
+1 3 11 2 8
+1 11 5 11 2
+9 8 12 3 1
+6 5 10 11 4
+1 10 11 2 9
+2 6 6 6 5
+1 6 11 3 6
+4 7 6 4 10'''
+
+lines = hidden_mod_keys_string.split("\n")
+keys = []
+starters = []
+finishers = []
+combos = []
+
+
+def match(key1, key2):
+    return key1[3] == key2[0] and key1[4] == key2[1]
+
+
+def combine(starter, middle, finisher):
+    starter_string = ' '.join(starter)
+    finisher_string = ' '.join(finisher)
+    combos.append(f'{starter_string} {middle[2]} {finisher_string}')
+
+
+for line in lines:
+    key = line.split()
+    keys.append(key)
+    if key[0] == '1':
+        starters.append(key)
+    if key[4] == '1':
+        finishers.append(key)
+
+for starter in starters:
+    for middle in keys:
+        if match(starter, middle):
+            for finisher in finishers:
+                if match(middle, finisher):
+                    combine(starter, middle, finisher)
+
+print(f'#combos {len(combos)}')
+print('combos:\n' + '\n'.join(combos))
+
+print(f'#starters {len(starters)}, #finishers {len(finishers)}')
