8a50a1c1ab3a258c2ddfeb26092cf2f356c917cc
Illia Halatiuk <galatyuk.ilya@gmail.com>
2022-08-07 17:17:21 +0700

    sync

--- 1.keyboard-mouse/src/EXAMPLE.py
+++ 1.keyboard-mouse/src/EXAMPLE.py
@@ -1,4 +1,5 @@
 import time
+from tkinter import messagebox
 
 import pyperclip
 
@@ -19,14 +20,15 @@ general = TapGroup([], "general")
 def open_screen_in_paint():
     Ct.release_all_keys()
     Ct.send("$(print_screen)")  # to get it in the clipboard
-    time.sleep(0.1)
+    time.sleep(0.3)
     Ct.send("$(windows)paint$(enter)")  # open everyone's favourite pic editor
-    time.sleep(0.1)
-    Ct.send("$(ctrl+v)$(esc)$(f11)")  # paste, esc to remove select, fullscreen
+    time.sleep(0.3)
+    Ct.send("$(ctrl+v)$(esc)")  # paste, esc to remove select
+    time.sleep(0.3)
+    Ct.send("$(f11)")  # fullscreen
 
 
 general.add({"shift+print_screen": open_screen_in_paint})
-
 
 mouse_pos_list = []
 
@@ -43,29 +45,46 @@ def finish_mouse_pos():
     mouse_pos_list = []
 
 
-general.add({  # doesn't work properly: app key opens menu on release. why?
-               # "app": "_", "shift+-": "",
-
-             # press f7 to collect mouse positions, then alt+f7 to copy list to clipboard, print and reset
-             "alt+f7": finish_mouse_pos, "f7": add_mouse_pos  # if swapped, alt+f7 won't work due to ordering
-             })
-
+general.add(
+    {  # doesn't work properly: app key opens menu on release. why?
+        # "app": "_", "shift+-": "",
+        # press f7 to collect mouse positions, then alt+f7 to copy list to clipboard, print and reset
+        "alt+f7": finish_mouse_pos,
+        "f7": add_mouse_pos,  # if swapped, alt+f7 won't work due to ordering
+    }
+)
 
 # only work if foobar is open but not necessarily in the front, disable if it's not.
 general.add(Tap("play_pause_media", "", trigger_if=lambda: not Ct.get_open(exec="foobar")))
 
-tapper.group({
-    "ctrl+y": "$(ctrl+shift+z)"
-}, "idea", trigger_if=lambda: Ct.get_fore(exec="idea"))
+tapper.group({"ctrl+y": "$(ctrl+shift+z)"}, "idea", trigger_if=lambda: Ct.get_fore(exec="idea"))
 
 
 def youtube_and_music_swap():
     Ct.send("$(play_pause_media) ")  # will start/stop music and stop/start video
 
 
-tapper.group([
-    Tap("play_pause_media", youtube_and_music_swap, trigger_if=lambda: Ct.get_open(exec="foobar"))
-], "chrome", trigger_if=lambda: Ct.get_fore(exec="chrome.exe"))
+tapper.group(
+    [
+        Tap("num5", "$(lmb)"),
+        Tap("num4", "$(rmb)"),
+        Tap("num8", "$(alt+f4+f4)"),
+        Tap("num0", lambda: messagebox.showinfo("mouse coords", str(Ct.get_mouse_pos()))),
+    ],
+    "mouse",
+)
+
+tapper.group(
+    [
+        Tap(
+            "play_pause_media",
+            youtube_and_music_swap,
+            trigger_if=lambda: Ct.get_open(exec="foobar") and Ct.get_fore(title="Youtube"),
+        )
+    ],
+    "chrome",
+    trigger_if=lambda: Ct.get_fore(exec="chrome.exe"),
+)
 
 tapper.add_groups([general])
 
