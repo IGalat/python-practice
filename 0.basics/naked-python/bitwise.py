1dc8d8173dc71c445132459b7a6dc1fa5637552b
Illia Halatiuk <galatyuk.ilya@gmail.com>
2024-04-29 14:56:02 +0700

    sync

--- 0.basics/naked-python/bitwise.py
+++
@@ -1,5 +0,0 @@
-def last_bit_is_1(x: int) -> None:
-    print(f"Last bit of {x} is 1? {x & 1}")
-
-
-[last_bit_is_1(x) for x in range(10)]
--- 2.backend-REST/flask-blog/src/rest/controllers.py
+++
@@ -1,7 +0,0 @@
-from rest.article_controller import article_controller
-from rest.comment_controller import comment_controller
-
-rest_controllers = {
-    "/articles": article_controller,
-    "/comments": comment_controller,
-}
